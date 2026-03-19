8ff6dfe4d0bd4b9dca8063404784dcecdce9d501
reiven1 <hswhsw5053@gmail.com>
2026-03-19 16:44:24 +0900

    base

---
+++ src/test/java/org/team12/teamproject/TeamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team12.teamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
