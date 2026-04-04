55b832226f4ac26b3a022ae5f3f1d054b020bc00
kyungmin Lee <akmwla1235@gmail.com>
2026-04-04 10:10:01 +0900

    대회 카드 정보 출력 에러 해결 백엔드

--- src/main/java/org/team12/teamproject/dto/CompetitionListResponseDto.java
+++ src/main/java/org/team12/teamproject/dto/CompetitionListResponseDto.java
@@ -3,6 +3,7 @@ package org.team12.teamproject.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
@@ -15,6 +16,7 @@ public class CompetitionListResponseDto {
     private String status;
     private LocalDateTime startAt;
     private LocalDateTime endAt;
+    private BigDecimal initialSeedMoney;
     private Integer maxParticipants;
     private Integer participantCount;
 }
