d55b5155360ad8129d37fd3585458fe545b9035b
KJM <dinf7605@gmail.com>
2026-04-06 21:16:46 +0900

    더미 데이터 제거 + 서버킬때 코스피,코스닥 주식 데이터 가져오기 추가 + 관심 종목 로직 추가

--- src/main/java/org/team12/teamproject/service/StockService.java
+++ src/main/java/org/team12/teamproject/service/StockService.java
@@ -52,83 +52,8 @@ public class StockService {
      */
     @PostConstruct
     public void initDummyData() {
-        if (stockRepository.count() == 0) {
-            String[] codes = {
-                    "005930", "삼성전자", "KOSPI",
-                    "000660", "SK하이닉스", "KOSPI",
-                    "373220", "LG에너지솔루션", "KOSPI",
-                    "207940", "삼성바이오로직스", "KOSPI",
-                    "005380", "현대차", "KOSPI",
-                    "000270", "기아", "KOSPI",
-                    "068270", "셀트리온", "KOSPI",
-                    "005490", "POSCO홀딩스", "KOSPI",
-                    "035420", "NAVER", "KOSPI",
-                    "051910", "LG화학", "KOSPI",
-                    "028260", "삼성물산", "KOSPI",
-                    "035720", "카카오", "KOSPI",
-                    "006400", "삼성SDI", "KOSPI",
-                    "105560", "KB금융", "KOSPI",
-                    "055550", "신한지주", "KOSPI",
-                    "012330", "현대모비스", "KOSPI",
-                    "003670", "포스코퓨처엠", "KOSPI",
-                    "086790", "하나금융지주", "KOSPI",
-                    "323410", "카카오뱅크", "KOSPI",
-                    "259960", "크래프톤", "KOSPI",
-                    "377300", "카카오페이", "KOSPI",
-                    "011200", "HMM", "KOSPI",
-                    "015760", "한국전력", "KOSPI",
-                    "018260", "삼성SDS", "KOSPI",
-                    "003490", "대한항공", "KOSPI",
-                    "034020", "두산에너빌리티", "KOSPI",
-                    "010130", "고려아연", "KOSPI",
-                    "009150", "삼성전기", "KOSPI",
-                    "032830", "삼성생명", "KOSPI",
-                    "316140", "우리금융지주", "KOSPI",
-                    "010950", "S-Oil", "KOSPI",
-                    "000810", "삼성화재", "KOSPI",
-                    "033920", "무학", "KOSPI",
-                    "011170", "롯데케미칼", "KOSPI",
-                    "024110", "기업은행", "KOSPI",
-                    "051900", "LG생활건강", "KOSPI",
-                    "138040", "메리츠금융지주", "KOSPI",
-                    "036570", "엔씨소프트", "KOSPI",
-                    "090430", "아모레퍼시픽", "KOSPI",
-                    "010140", "삼성중공업", "KOSPI",
-                    "028050", "삼성엔지니어링", "KOSPI",
-                    "086280", "현대글로비스", "KOSPI",
-                    "021240", "코웨이", "KOSPI",
-                    "022100", "포스코DX", "KOSDAQ",
-                    "247540", "에코프로비엠", "KOSDAQ",
-                    "086520", "에코프로", "KOSDAQ",
-                    "066970", "엘앤에프", "KOSDAQ",
-                    "028300", "HLB", "KOSDAQ",
-                    "196170", "알테오젠", "KOSDAQ",
-                    "293490", "카카오게임즈", "KOSDAQ",
-                    "058470", "리노공업", "KOSDAQ",
-                    "278280", "천보", "KOSDAQ",
-                    "039030", "이오테크닉스", "KOSDAQ",
-                    "041510", "에스엠", "KOSDAQ",
-                    "035900", "JYP Ent.", "KOSDAQ",
-                    "122870", "와이지엔터테인먼트", "KOSDAQ",
-                    "036490", "SK머티리얼즈", "KOSDAQ",
-                    "214150", "클래시스", "KOSDAQ",
-                    "000250", "삼천당제약", "KOSDAQ",
-                    "034230", "파라다이스", "KOSDAQ"
-            };
-
-            for (int i = 0; i < codes.length; i += 3) {
-                stockRepository.save(
-                        Stock.builder()
-                                .stockCode(codes[i])
-                                .stockName(codes[i + 1])
-                                .marketType(codes[i + 2])
-                                .isActive(true)
-                                .createdAt(LocalDateTime.now())
-                                .build());
-            }
-
-            log.info("주식 더미 데이터 삽입 완료");
-        }
+        // KisMasterSyncService를 통해 전 종목을 동기화하므로 더 이상 더미 데이터를 사용하지 않습니다.
+        // log.info("더미 데이터 대신 전체 KIS 마스터 파일을 동기화합니다.");
     }
 
     /**
