7799bf8740ac3c6e5b8978dfeebab78a7e898aab
reiven1 <hswhsw5053@gmail.com>
2026-04-26 13:25:24 +0900

    도메인 적용 후 cors 설정

--- src/main/java/org/team12/teamproject/WebConfig.java
+++ src/main/java/org/team12/teamproject/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5174", "http://localhost:3000", "https://mockinvest.kro.kr", "http://mockinvest.kro.kr")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
